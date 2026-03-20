936ee00fa64e77eea2c341ba0981c492a2987826
Claudio Gaete <claudio.gaete@ug.uchile.cl>
2026-03-20 16:31:54 -0300

    fix: hacer origins inmutable

--- xmin/origins.py
+++ xmin/origins.py
@@ -8,6 +8,21 @@ class Origins:
     """
     Clase que define las regiones para las cuales se calcularán índices de
     accesibilidad.
+
+    Parameters
+    ---
+    bounds : GeoDataFrame
+        GeoDataFrame con los límites de las regiones para las cuales se
+        calcularán índices de accesibilidad
+    h3_level : int, default: 8
+        Nivel de H3 que se utilizará para dividir las celdas. Más
+        información en https://h3geo.org/docs/core-library/restable/
+    population_gdf : GeoDataFrame or None, default: None
+        GeoDataFrame con datos de población. Debe tener por lo menos las
+        columnas `population` y `geometry`. No es necesario que cubra el
+        mismo área que `bounds`. Si no se recibe un GeoDataFrame, no se
+        cargarán datos de población en los orígenes y no se permitirá el
+        uso de índices que necesiten esa información.
     """
 
     def __init__(
@@ -16,27 +31,10 @@ class Origins:
         h3_level: int = 8,
         population_gdf: gpd.GeoDataFrame | None = None,
     ):
-        """
-        Parameters
-        ---
-        bounds : GeoDataFrame
-            GeoDataFrame con los límites de las regiones para las cuales se
-            calcularán índices de accesibilidad
-        h3_level : int, default: 8
-            Nivel de H3 que se utilizará para dividir las celdas. Más
-            información en https://h3geo.org/docs/core-library/restable/
-        population_gdf : GeoDataFrame or None, default: None
-            GeoDataFrame con datos de población. Debe tener por lo menos las
-            columnas `population` y `geometry`. No es necesario que cubra el
-            mismo área que `bounds`. Si no se recibe un GeoDataFrame, no se
-            cargarán datos de población en los orígenes y no se permitirá el
-            uso de índices que necesiten esa información.
-        """
 
-        self.bounds = bounds
-        self.h3_level = h3_level
+        self._bounds = bounds
 
-        self.h3_grid = h3fy(bounds, resolution=self.h3_level)
+        self._h3_grid = h3fy(bounds, resolution=h3_level)
         if population_gdf is not None:
             if not (
                 "population" in population_gdf.columns
@@ -47,7 +45,7 @@ class Origins:
                     "`population` y `geometry`"
                 )
             self._overlay_population_h3(population_gdf)
-        self.h3_grid = self.h3_grid.rename_axis("id").reset_index()
+        self._h3_grid = self._h3_grid.rename_axis("id").reset_index()
 
     def _overlay_population_h3(
         self, population_gdf: gpd.GeoDataFrame
@@ -68,7 +66,7 @@ class Origins:
         # overlay zones with H3 cells, splitting zones that fall between
         # multiple cells into multiple "fragments"
         population_gdf_split = population_gdf.overlay(
-            self.h3_grid.reset_index()
+            self._h3_grid.reset_index()
         )
 
         # assign population to each zone fragment, proportional to the area of
@@ -84,4 +82,14 @@ class Origins:
             "population_split"
         ].sum()
 
-        self.h3_grid["population"] = h3_population.fillna(0)
+        self._h3_grid["population"] = h3_population.fillna(0)
+
+    @property
+    def bounds(self) -> gpd.GeoDataFrame:
+        """GeoDataFrame con los límites de las regiones."""
+        return self._bounds
+
+    @property
+    def h3_grid(self) -> gpd.GeoDataFrame:
+        """GeoDataFrame con las celdas hexagonales de las regiones."""
+        return self._h3_grid
